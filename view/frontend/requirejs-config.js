9bf837f8da07b349a3457abc2a94d35f1bc83686
Lucas Calazans <calazans95@hotmail.com>
2018-08-06 14:19:58 -0300

    Initial Commit

---
+++ view/frontend/requirejs-config.js
@@ -0,0 +1,6 @@
+var config = {
+    paths: {
+        es6: "LucasCalazans_EcmaScript/node_modules/requirejs-babel/es6",
+        babel: "LucasCalazans_EcmaScript/node_modules/requirejs-babel/babel-5.8.34.min"
+    }
+};
